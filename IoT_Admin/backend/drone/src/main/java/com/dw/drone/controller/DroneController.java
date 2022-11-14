27917facfcaa670b1e3429cd6da2575da9eb2b47
sds2013 <111099668+sds2013@users.noreply.github.com>
2022-11-15 08:54:36 +0900

    연습한거

---
+++ IoT_Admin/backend/drone/src/main/java/com/dw/drone/controller/DroneController.java
@@ -0,0 +1,5 @@
+package com.dw.drone.controller;
+
+public class DroneController {
+
+}
---
+++ IoT_Admin/backend/drone/src/main/java/com/dw/drone/service/MainService.java
@@ -0,0 +1,8 @@
+package com.dw.drone.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainService {
+
+}
